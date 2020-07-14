bf0b33f26123428a8d73d258bbf7cced1518f17e
Sundorius <manos_katsif@hotmail.com>
2020-07-14 13:39:07 +0300

    Removed doc file
    Removed doc file

--- Chess/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"chess"},{"l":"main"},{"l":"pieces"},{"l":"player"}]
--- Chess/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"pieces","l":"Bishop"},{"p":"chess","l":"Chess"},{"p":"chess","l":"EvaluateResult"},{"p":"pieces","l":"King"},{"p":"main","l":"Main"},{"p":"chess","l":"Node"},{"p":"pieces","l":"Pawn"},{"p":"pieces","l":"Piece"},{"p":"player","l":"Player"},{"p":"pieces","l":"Queen"},{"p":"pieces","l":"Rook"}]
